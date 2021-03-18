037a145938f25ac7405c5369c2268e649416a3d0
Mateusz Zawiski <mzawiski97@gmail.com>
2021-03-18 23:05:58 +0100

    Implemented mutation method

---
+++ GeneticAlgorithm/Mutation/BorderMutation.cs
@@ -0,0 +1,43 @@
+﻿using GeneticAlgorithm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneticAlgorithm.Mutation
+{
+    public class BorderMutation : IMutation
+    {
+        public float Probability { get; init; }
+
+        public BorderMutation(float probability)
+        {
+            Probability = probability;
+        }
+
+        public IEnumerable<Entity> Mutate(IEnumerable<Entity> population)
+        {
+            var mutatedPopulation = new List<Entity>();
+
+            foreach(var entity in mutatedPopulation)
+            {
+                if (new Random().NextDouble() <= Probability)
+                {
+                    mutatedPopulation.Add(MutateEntity(entity));
+                    continue;
+                }
+
+                mutatedPopulation.Add(entity);
+            }
+
+            return mutatedPopulation;
+            
+        }
+
+        private static Entity MutateEntity(Entity entity)
+        {
+            var newChromosome = entity.Chromosome.Extract(0, (uint)entity.Chromosome.Genes.Length - 1) + new Chromosome(new[] { !entity.Chromosome.Genes.Last() });
+
+            return entity.Reproduct(newChromosome);
+        }
+    }
+}
---
+++ GeneticAlgorithm/Mutation/IMutation.cs
@@ -0,0 +1,11 @@
+﻿using GeneticAlgorithm.Models;
+using System.Collections.Generic;
+
+namespace GeneticAlgorithm.Mutation
+{
+    interface IMutation
+    {
+        public float Probability { get; init; }
+        IEnumerable<Entity> Mutate(IEnumerable<Entity> population);
+    }
+}
