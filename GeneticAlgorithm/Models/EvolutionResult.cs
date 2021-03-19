01d724e6aad45b7ad96181cd1c4ef1bad6d19802
Mateusz Zawiski <mzawiski97@gmail.com>
2021-03-19 21:42:14 +0100

    Added evolution result object

---
+++ GeneticAlgorithm/Models/EvolutionResult.cs
@@ -0,0 +1,11 @@
+﻿using OxyPlot;
+
+namespace GeneticAlgorithm.Models
+{
+    public class EvolutionResult
+    {
+        public DataPoint Indexes { get; set; } = new();
+        public DataPoint Best { get; set; } = new();
+        public Point[] Points { get; set; }
+    }
+}
